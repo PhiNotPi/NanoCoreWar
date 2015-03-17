52679392a17acb99228a19976923af38f96eea66
Ilmari Karonen <nospam@vyznev.net>
2015-03-17 21:38:53 +0200

    inline the execute() method

--- Game.java
+++ Game.java
@@ -102,104 +102,113 @@ public class Game
             if(core[i] == null) core[i] = new Instruction();
         }
              
-        int curOffset = offset1 & coreSizeM1, curLoc = curOffset;
-        int altOffset = offset2 & coreSizeM1, altLoc = altOffset;
+        int poffset = offset1 & coreSizeM1, ploc = poffset;
+        int xoffset = offset2 & coreSizeM1, xloc = xoffset;
 
         int maxSteps = maxTime * 2;
         for(int step = 0; step != maxSteps; step++)
         {
             if(debug != 0)
             {
-                printCore(core, curLoc, altLoc, step);
+                printCore(core, ploc, xloc, step);
             }
             
-            Instruction curInsrt = core[curLoc];
-            if(curInsrt.packedOp < Instruction.minValidOp)
+            Instruction curr = core[ploc];
+            int op = curr.packedOp;
+            if(op < Instruction.minValidOp)
             {
                 return ((step & 1) == 0 ? 0 : 2);
             }
-            curLoc = execute(core, curInsrt, curLoc, curOffset);
             
-            int tmpLoc = curLoc; curLoc = altLoc; altLoc = tmpLoc;            
-            int tmpOffset = curOffset; curOffset = altOffset; altOffset = tmpOffset;            
+            int line1 = curr.field1, line2 = curr.field2;
+            int opcode = op >> (Instruction.modeBits*2), mode1 = (op >> Instruction.modeBits), mode2 = op;
+            mode1 &= Instruction.modeMask;
+            mode2 &= Instruction.modeMask;
+
+            switch(mode1)
+            {
+            case Instruction.MODE_IMM:
+                line1 += poffset;
+                break;
+            case Instruction.MODE_DIR:
+                line1 += ploc;
+                break;
+            case Instruction.MODE_IND:
+                line1 += ploc;
+                line1 += core[line1 & coreSizeM1].field1;
+                break;
+            default:
+                throw new IllegalStateException("invalid A addressing mode " + mode1 + " (decoded from " + op + ") on line " + ploc);
+            }
+            
+            switch(mode2)
+            {
+            case Instruction.MODE_IMM:
+                line2 += poffset;
+                break;
+            case Instruction.MODE_DIR:
+                line2 += ploc;
+                break;
+            case Instruction.MODE_IND:
+                line2 += ploc;
+                line2 += core[line2 & coreSizeM1].field2;
+                break;
+            default:
+                throw new IllegalStateException("invalid B addressing mode " + mode2 + " (decoded from " + op + ") on line " + ploc);
+            }
+            
+            line1 &= coreSizeM1;
+            line2 &= coreSizeM1;
+            Instruction i1, i2;
+     
+            switch(opcode)
+            {
+            case Instruction.OP_MOV:
+                i1 = core[line1]; i2 = core[line2];
+                i2.packedOp = i1.packedOp;
+                i2.field1 = i1.field1;
+                i2.field2 = i1.field2;
+                break;
+            case Instruction.OP_ADD:
+                i1 = core[line1]; i2 = core[line2];
+                i2.field1 += i1.field1;
+                i2.field2 += i1.field2;
+                break;
+            case Instruction.OP_SUB:
+                i1 = core[line1]; i2 = core[line2];
+                i2.field1 -= i1.field1;
+                i2.field2 -= i1.field2;
+                break;
+            case Instruction.OP_JMP:
+                ploc = line1 - 1;  // will be incremented by one below
+                break;
+            case Instruction.OP_JMZ:
+                i2 = core[line2];
+                if(i2.field1 == 0 && i2.field2 == 0)
+                {
+                    ploc = line1 - 1;
+                }
+                break;
+            case Instruction.OP_CMP:
+                i1 = core[line1]; i2 = core[line2];
+                if(i1.field1 != i2.field1 || i1.field2 != i2.field2)
+                {
+                    ploc++;
+                }
+                break;
+            default:
+                throw new IllegalStateException("invalid opcode " + opcode + " (decoded from " + op + ") on line " + ploc);
+            }
+            
+            ploc++;
+            ploc &= coreSizeM1;
+           
+            int tmpLoc = ploc; ploc = xloc; xloc = tmpLoc;            
+            int tmpOffset = poffset; poffset = xoffset; xoffset = tmpOffset;            
         }
         return 1;
     }
-    public int execute(Instruction[] core, Instruction curr, int ploc, int offset)
-    {
-        int op = curr.packedOp, line1 = curr.field1, line2 = curr.field2;
-        int opcode = op >> (Instruction.modeBits*2), mode1 = (op >> Instruction.modeBits), mode2 = op;
-        mode1 &= Instruction.modeMask;
-        mode2 &= Instruction.modeMask;
-
-        switch(mode1)
-        {
-        case Instruction.MODE_IMM:
-            line1 += offset;
-            break;
-        case Instruction.MODE_DIR:
-            line1 += ploc;
-            break;
-        case Instruction.MODE_IND:
-            line1 += ploc;
-            line1 += core[line1 & coreSizeM1].field1;
-            break;
-        default:
-            throw new IllegalStateException("invalid A addressing mode " + mode1 + " (decoded from " + op + ") on line " + ploc);
-        }
-        
-        switch(mode2)
-        {
-        case Instruction.MODE_IMM:
-            line2 += offset;
-            break;
-        case Instruction.MODE_DIR:
-            line2 += ploc;
-            break;
-        case Instruction.MODE_IND:
-            line2 += ploc;
-            line2 += core[line2 & coreSizeM1].field2;
-            break;
-        default:
-            throw new IllegalStateException("invalid B addressing mode " + mode2 + " (decoded from " + op + ") on line " + ploc);
-        }
-        
-        line1 &= coreSizeM1;
-        line2 &= coreSizeM1;
-        int next = (ploc+1) & coreSizeM1;
-        Instruction i1, i2;
-
-        switch(opcode)
-        {
-        case Instruction.OP_MOV:
-            i1 = core[line1]; i2 = core[line2];
-            i2.packedOp = i1.packedOp;
-            i2.field1 = i1.field1;
-            i2.field2 = i1.field2;
-            return next;
-        case Instruction.OP_ADD:
-            i1 = core[line1]; i2 = core[line2];
-            i2.field1 += i1.field1;
-            i2.field2 += i1.field2;
-            return next;
-        case Instruction.OP_SUB:
-            i1 = core[line1]; i2 = core[line2];
-            i2.field1 -= i1.field1;
-            i2.field2 -= i1.field2;
-            return next;
-        case Instruction.OP_JMP:
-            return line1;
-        case Instruction.OP_JMZ:
-            i2 = core[line2];
-            return (i2.field1 == 0 && i2.field2 == 0 ? line1 : next);
-        case Instruction.OP_CMP:
-            i1 = core[line1]; i2 = core[line2];
-            return next + (i1.field1 != i2.field1 || i1.field2 != i2.field2 ? 1 : 0);
-        default:
-            throw new IllegalStateException("invalid opcode " + opcode + " (decoded from " + op + ") on line " + ploc);
-        }
-    }
-    public void printCore(Instruction[] core, int curLoc, int altLoc, int step)
+    public void printCore(Instruction[] core, int ploc, int xloc, int step)
     {
         int time = (step >> 1), turn = (step & 1);
         
@@ -208,7 +217,7 @@ public class Game
         for(int i = 0; i < core.length; i++)
         {
             Instruction line = core[i];
-            if(line.equals(dupLine) && i != curLoc && i != altLoc)
+            if(line.equals(dupLine) && i != ploc && i != xloc)
             {
                 if(dupCount == 0)
                 {
@@ -227,11 +236,11 @@ public class Game
                     System.out.println("    " + (dupCount - 1) + " lines skipped.");
                 }
                 System.out.print(line);
-                if(i == curLoc)
+                if(i == ploc)
                 {
                     System.out.print("\t<- " + (turn == 0 ? 1 : 2));
                 }
-                if(i == altLoc)
+                if(i == xloc)
                 {
                     System.out.print("\t<- " + (turn == 0 ? 2 : 1));
                 }
